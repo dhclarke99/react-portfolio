df2d6ccfff756a94f6abd4a5e38b247c0dad0deb
dhclarke99 <dhclarke99@gmail.com>
2023-05-22 15:39:15 -0400

    updated portfolio tab layout

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,23 +3,49 @@ import logo from '../../images/logo192.png';
 
 export default function Portfolio() {
   return (
-    <div>
+    <div className="container-fluid">
       <h1>Portfolio</h1>
-      <img src={logo} alt="Project 1">
+      <br></br>
+      <div>
+        <h2>Tech Blog</h2>
+      <a href="https://github.com/dhclarke99/tech-blog">
+      <img src={logo} alt="Tech Blog">
       
       </img>
-      <img src="" alt="Project 2">
-        
+      </a>
+      </div>
+      <div>
+      <h2>BucketList Generator</h2>
+      <a href="https://github.com/dhclarke99/bucketlist-generator">
+      <img src={logo} alt="BucketList Generator">
+      
       </img>
-      <img src="" alt="Project 3">
-        
+      </a>
+      </div>
+      <div>
+      <h2>Date Planner</h2>
+      <a href="https://github.com/dhclarke99/date_planner">
+      <img src={logo} alt="Date Planner">
+      
       </img>
-      <img src="" alt="Project 4">
-        
+      </a>
+      </div>
+      <div>
+      <h2>Weather Dashboard</h2>
+      <a href="https://github.com/dhclarke99/weather-dashboard">
+      <img src={logo} alt="Weather Dashboard">
+      
       </img>
-      <img src="" alt="Project 5">
-        
+      </a>
+      </div>
+      <div>
+      <h2>ExpressJS NoteTaker</h2>
+      <a href="https://github.com/dhclarke99/expressjs-note-taker">
+      <img src={logo} alt="ExpressJS NoteTaker">
+      
       </img>
+      </a>
+      </div>
     </div>
   );
 }
