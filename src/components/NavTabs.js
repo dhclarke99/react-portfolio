a874924788af00d9b92a532d8018cdfa9af9a42a
dhclarke99 <dhclarke99@gmail.com>
2023-05-22 15:01:24 -0400

    added image tags in portfolio tab

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// TODO: Add a comment explaining how we are able to extract the key value pairs from props
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
@@ -10,7 +9,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#about"
           onClick={() => handlePageChange('About')}
-          //  TODO: Add a comment explaining what this logic is doing
+          
 
           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
         >
@@ -22,7 +21,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#portfolio"
           onClick={() => handlePageChange('Portfolio')}
-          //  TODO: Add a comment explaining what this logic is doing
+       
 
           className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
         >
@@ -32,7 +31,7 @@ function NavTabs({ currentPage, handlePageChange }) {
       <li className="nav-item">
         <a
           href="#contact"
-          //  TODO: Add a comment explaining what this logic is doing
+         
 
           onClick={() => handlePageChange('Contact')}
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
@@ -44,7 +43,6 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#resume"
           onClick={() => handlePageChange('Resume')}
-          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
 
           className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
         >
