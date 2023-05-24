d4e5b2044c759d2b0021da557045c66dc22f5aec
dhclarke99 <dhclarke99@gmail.com>
2023-05-23 21:08:08 -0400

    added content and styles to resume tab

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,17 +5,20 @@ export default function Resume() {
     <div id="tab-content">
       <h1>Resume</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+        Download my Resume
       </p>
+      <h3>Front-End Proficiencies</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+      </ul>
+      <h3>Back-End Proficiencies</h3>
+      <ul>
+        <li>MySQL</li>
+        <li>MongoDB</li>
+        <li>ExpressJS</li>
+      </ul>
     </div>
   );
 }
