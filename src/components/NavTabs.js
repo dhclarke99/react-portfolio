b6524b50d17f674f212272bf82ff1b8b90fb44df
dhclarke99 <dhclarke99@gmail.com>
2023-05-23 22:23:04 -0400

    added style to nav items

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
+    <ul className="nav">
       
       <li className="nav-item">
         <a
