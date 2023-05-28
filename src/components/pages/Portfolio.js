2dc94dae1488e9a41bdaa648a3db0ea1b0b38dc0
dhclarke99 <dhclarke99@gmail.com>
2023-05-28 13:45:21 -0400

    updated code to create a single Portfolio component and map through an array of portfolio data

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,62 +1,20 @@
 import React from 'react';
-import logo from '../../images/logo192.png';
-import techBlog from '../../images/Tech Blog.png';
-import bucketList from '../../images/BucketList.png';
-import datePlanner from '../../images/DatePlanner.png';
-import weatherDashboard from '../../images/WeatherDashboard.png';
-import noteTaker from '../../images/NoteTaker.png';
-
+import portfolioData from './portfolioData';
 
 export default function Portfolio() {
   return (
     <div className="container" id="tab-content">
       <h1>Portfolio</h1>
-      <br></br>
+      <br />
       <div className="row portfolio">
-      <div className="col portfolio-image">
-        <h2>Tech Blog</h2>
-      <a href="https://github.com/dhclarke99/tech-blog">
-      <img src={techBlog} alt="Tech Blog" >
-      
-      </img>
-      </a>
-      </div>
-      <div className="col portfolio-image">
-      <h2>BucketList Generator</h2>
-      <a href="https://github.com/dhclarke99/bucketlist-generator">
-      <img src={bucketList} alt="BucketList Generator">
-      
-      </img>
-      </a>
-      </div>
-      </div>
-      <div className="row portfolio">
-      <div className="col portfolio-image">
-      <h2>Date Planner</h2>
-      <a href="https://github.com/dhclarke99/date_planner">
-      <img src={datePlanner} alt="Date Planner">
-      
-      </img>
-      </a>
-      </div>
-      <div className="col portfolio-image">
-      <h2>Weather Dashboard</h2>
-      <a href="https://github.com/dhclarke99/weather-dashboard">
-      <img src={weatherDashboard} alt="Weather Dashboard">
-      
-      </img>
-      </a>
-      </div>
-      </div>
-      <div className="row portfolio">
-      <div className="col portfolio-image">
-      <h2>ExpressJS NoteTaker</h2>
-      <a href="https://github.com/dhclarke99/expressjs-note-taker">
-      <img src={noteTaker} alt="ExpressJS NoteTaker">
-      
-      </img>
-      </a>
-      </div>
+        {portfolioData.map((item, index) => (
+          <div className="col portfolio-image" key={index}>
+            <h2>{item.title}</h2>
+            <a href={item.link}>
+              <img src={item.image} alt={item.title} />
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/components/pages/portfolioData.js
@@ -0,0 +1,38 @@
+
+import techBlog from '../../images/Tech Blog.png';
+import bucketList from '../../images/BucketList.png';
+import datePlanner from '../../images/DatePlanner.png';
+import weatherDashboard from '../../images/WeatherDashboard.png';
+import noteTaker from '../../images/NoteTaker.png';
+
+
+
+const portfolioData = [
+    {
+      title: 'Tech Blog',
+      image: techBlog,
+      link: 'https://github.com/dhclarke99/tech-blog',
+    },
+    {
+      title: 'BucketList Generator',
+      image: bucketList,
+      link: 'https://github.com/dhclarke99/bucketlist-generator',
+    },
+    {
+      title: 'Date Planner',
+      image: datePlanner,
+      link: 'https://github.com/dhclarke99/date_planner',
+    },
+    {
+      title: 'Weather Dashboard',
+      image: weatherDashboard,
+      link: 'https://github.com/dhclarke99/weather-dashboard',
+    },
+    {
+      title: 'ExpressJS NoteTaker',
+      image: noteTaker,
+      link: 'https://github.com/dhclarke99/expressjs-note-taker',
+    },
+  ];
+
+  export default portfolioData;
