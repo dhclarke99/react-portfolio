69e3af69c5d9fc85e577c7a826e56409aa31a6b9
dhclarke99 <dhclarke99@gmail.com>
2023-06-01 16:03:52 -0400

    updated Portfolio component

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,20 +2,29 @@ import React from 'react';
 import portfolioData from './portfolioData';
 
 export default function Portfolio() {
+  const portfolioItems = portfolioData.map((item, index) => (
+    <div className="col portfolio-image" key={index}>
+      <h2>{item.title}</h2>
+      <a href={item.link}>
+        <img src={item.image} alt={item.title} />
+      </a>
+    </div>
+  ));
+
+  const dividedPortfolioItems = [];
+  for (let i = 0; i < portfolioItems.length; i += 2) {
+    dividedPortfolioItems.push(
+      <div className="row portfolio" key={i}>
+        {portfolioItems.slice(i, i + 2)}
+      </div>
+    );
+  }
+
   return (
-    <div className="container" id="tab-content">
+    <div className="col container" id="tab-content">
       <h1>Portfolio</h1>
       <br />
-      <div className="row portfolio">
-        {portfolioData.map((item, index) => (
-          <div className="col portfolio-image" key={index}>
-            <h2>{item.title}</h2>
-            <a href={item.link}>
-              <img src={item.image} alt={item.title} />
-            </a>
-          </div>
-        ))}
-      </div>
+      {dividedPortfolioItems}
     </div>
   );
 }
