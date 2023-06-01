b5768297bf79e743c1706cc09eb14d7e02888ab7
dhclarke99 <dhclarke99@gmail.com>
2023-06-01 16:07:41 -0400

    added hover effect to portfolio images

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import portfolioData from './portfolioData';
 
 export default function Portfolio() {
-  const portfolioItems = portfolioData.map((item, index) => (
-    <div className="col portfolio-image" key={index}>
-      <h2>{item.title}</h2>
-      <a href={item.link}>
-        <img src={item.image} alt={item.title} />
-      </a>
-    </div>
-  ));
+  const [hoveredItem, setHoveredItem] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHoveredItem(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredItem(null);
+  };
 
   const dividedPortfolioItems = [];
-  for (let i = 0; i < portfolioItems.length; i += 2) {
+  for (let i = 0; i < portfolioData.length; i += 2) {
     dividedPortfolioItems.push(
       <div className="row portfolio" key={i}>
-        {portfolioItems.slice(i, i + 2)}
+        {portfolioData.slice(i, i + 2).map((item, index) => (
+          <div
+            className="col portfolio-image"
+            key={index}
+            onMouseEnter={() => handleMouseEnter(i + index)}
+            onMouseLeave={handleMouseLeave}
+          >
+            <a href={item.link}>
+              <img src={item.image} alt={item.title} />
+              {hoveredItem === i + index && (
+                <div className="portfolio-overlay">
+                  <h2>{item.title}</h2>
+                </div>
+              )}
+            </a>
+          </div>
+        ))}
       </div>
     );
   }
 
   return (
-    <div className="col container" id="tab-content">
+    <div className="container" id="tab-content">
       <h1>Portfolio</h1>
       <br />
       {dividedPortfolioItems}
