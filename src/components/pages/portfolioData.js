b1bc8d2e7ae75a895567b68eee5fe81b4538d985
dhclarke99 <dhclarke99@gmail.com>
2023-06-01 21:39:36 -0400

    optimized for mobile, updated images and links in portfolio tab

--- src/components/pages/portfolioData.js
+++ src/components/pages/portfolioData.js
@@ -11,32 +11,32 @@ const portfolioData = [
     {
       title: 'Tech Blog',
       image: techBlog,
-      link: 'https://github.com/dhclarke99/tech-blog',
+      link: 'https://safe-stream-47655.herokuapp.com/',
       gitHubLink: 'https://github.com/dhclarke99/tech-blog'
     },
     {
       title: 'BucketList Generator',
       image: bucketList,
-      link: 'https://github.com/dhclarke99/bucketlist-generator',
-      gitHubLink: 'https://github.com/dhclarke99/tech-blog'
+      link: 'https://pacific-beach-42449.herokuapp.com/',
+      gitHubLink: 'https://github.com/dhclarke99/bucketlist-generator'
     },
     {
       title: 'Date Planner',
       image: datePlanner,
-      link: 'https://github.com/dhclarke99/date_planner',
-      gitHubLink: 'https://github.com/dhclarke99/tech-blog'
+      link: 'https://dhclarke99.github.io/date_planner/',
+      gitHubLink: 'https://github.com/dhclarke99/date_planner'
     },
     {
       title: 'Weather Dashboard',
       image: weatherDashboard,
-      link: 'https://github.com/dhclarke99/weather-dashboard',
-      gitHubLink: 'https://github.com/dhclarke99/tech-blog'
+      link: 'https://dhclarke99.github.io/weather-dashboard/',
+      gitHubLink: 'https://github.com/dhclarke99/weather-dashboard'
     },
     {
       title: 'ExpressJS NoteTaker',
       image: noteTaker,
-      link: 'https://github.com/dhclarke99/expressjs-note-taker',
-      gitHubLink: 'https://github.com/dhclarke99/tech-blog'
+      link: 'https://serene-hamlet-96189.herokuapp.com/',
+      gitHubLink: 'https://github.com/dhclarke99/expressjs-note-taker'
     },
   ];
 
