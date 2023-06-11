731bf829afb98331845869304e119b47cb8cbf3a
dhclarke99 <dhclarke99@gmail.com>
2023-06-11 13:31:39 -0400

    added linkedin to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/style.css';
 import gitHubLogo from '../images/gitHubLogo.png';
+import linkedinLogo from '../images/linkedinLogo.png';
 
 
 export default function Footer() {
@@ -13,9 +14,9 @@ return (
             </img>
           </li>
           </a>
-          <a href="https://github.com/dhclarke99">
+          <a href="https://www.linkedin.com/in/david-clarke-17355a185/">
           <li>
-            <img src={gitHubLogo} alt="GitHub Logo">
+            <img src={linkedinLogo} alt="GitHub Logo">
             </img>
           </li>
           </a>
